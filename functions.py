76ac296a2747395bff91fbaccd57b0711f264871
Lamas Neil <neillamas@gmail.com>
2024-02-20 22:32:52 +0100

    Finished converting whole program but plotting

--- functions.py
+++ functions.py
@@ -1,53 +1,122 @@
 import numpy as np
 import pandas as pd
+import os
+import glob
+from lmfit import Parameters, Model
+import matplotlib.pyplot as plt
+
+
+def lorentzian_fitting(df, Q_loaded_init, res_init):
+    magnitude = 10 ** (df["re:S21"].values / 20)
+    S21res = magnitude.max()
+
+    params = Parameters()
+    params.add("a", value=S21res, min=S21res - 0.1, max=S21res + 0.1)
+    params.add("b", value=Q_loaded_init, min=0, max=Q_loaded_init * (1.01))
+    params.add("c", value=res_init, min=res_init - 1e8, max=res_init + 1e8)
+    params.add("d", value=0, min=-10, max=10)
+    params.add("e", value=0, min=-10, max=10)
+
+    # Define fitting function
+    def fun(x, a, b, c, d, e):
+        return abs(a / (1 + 2 * 1j * b * ((x - c) / c)) + d + 1j * e)
+
+    model = Model(fun)
+
+    f = df["freq[Hz]"].values
+    result_fitting = model.fit(magnitude, params, x=f, method="leastsq")
+    best_fit_values = result_fitting.best_fit
+
+    # Plot data points and the fitted curve
+    plt.plot(f, magnitude, "x-", linewidth=1.2, label="Data")
+    plt.plot(f, best_fit_values, "m", label="Fit")
+
+    plt.show()
+
+    # CONSIDERS IT BOTH UNLOADED AND LOADED IN MATLAB file
+    Q_loaded_fit = result_fitting.params["b"].value
+    resonance_fit = result_fitting.params["c"].value
+
+    return [Q_loaded_fit, resonance_fit]
 
 
 def DR_calculation(df):
-    idx_reS21_max = df['re:S21'].idxmax()
-    reS21_max = df['re:S21'].max()
+    idx_reS21_max = df["re:S21"].idxmax()
+    reS21_max = df["re:S21"].max()
 
-    f1 = np.interp(-3.01, df['re:S21'].iloc[:idx_reS21_max+1] - reS21_max, df['freq[Hz]'].iloc[:idx_reS21_max+1])
-    f2 = np.interp(-3.01, df['re:S21'].iloc[idx_reS21_max+1:] - reS21_max, df['freq[Hz]'].iloc[idx_reS21_max+1:])
+    f1 = np.interp(
+        -3.01,
+        df["re:S21"].iloc[: idx_reS21_max + 1] - reS21_max,
+        df["freq[Hz]"].iloc[: idx_reS21_max + 1],
+    )
+    f2 = np.interp(
+        -3.01,
+        df["re:S21"].iloc[idx_reS21_max + 1 :] - reS21_max,
+        df["freq[Hz]"].iloc[idx_reS21_max + 1 :],
+    )
 
-    resonance = df.loc[idx_reS21_max, 'freq[Hz]']
-    Qloaded = resonance/(f2 - f1)
+    resonance = df.loc[idx_reS21_max, "freq[Hz]"]
+    Qloaded = resonance / (f2 - f1)
 
-    df['magnitudeS11'] = df['re:S11'] - ((df['re:S11'].iloc[0]+df['re:S11'].iloc[-1])*0.5)
-    df['magnitudeS22'] = df['re:S22'] - ((df['re:S22'].iloc[0]+df['re:S22'].iloc[-1])*0.5)
+    df["magnitudeS11"] = df["re:S11"] - (
+        (df["re:S11"].iloc[0] + df["re:S11"].iloc[-1]) * 0.5
+    )
+    df["magnitudeS22"] = df["re:S22"] - (
+        (df["re:S22"].iloc[0] + df["re:S22"].iloc[-1]) * 0.5
+    )
 
     # S11resdb = df['magnitudeS11'].min()
     # S21resdb = df['re:S21'].max()
-    S22resdb = df.loc[idx_reS21_max, 'magnitudeS22']
+    S22resdb = df.loc[idx_reS21_max, "magnitudeS22"]
 
-    print(df['magnitudeS11'].max())
-    print(df['magnitudeS11'].max()/20)
+    S11res = 10 ** (df["magnitudeS11"].max() / 20)
+    S21res = 10 ** (df["re:S21"].max() / 20)
+    print(S21res)
 
-    S11res = 10**(df['magnitudeS11'].max()/20)
-    # S21res = 10**(df['re:S21'].max()/20)
-    S22res = 10**(S22resdb/20)
+    S22res = 10 ** (S22resdb / 20)
 
-    beta1 = (1-S11res)/(S11res+S22res)
-    beta2 = (1-S22res)/(S11res+S22res)
+    beta1 = (1 - S11res) / (S11res + S22res)
+    beta2 = (1 - S22res) / (S11res + S22res)
 
-    Qunloaded = Qloaded*(1+beta1+beta2)
+    Qunloaded = Qloaded * (1 + beta1 + beta2)
 
-    return [Qloaded, Qunloaded, resonance, beta1, beta2]
+    return [df, Qloaded, Qunloaded, resonance, beta1, beta2]
 
 
-def read_touchstone(filename):
-    with open(filename, 'r') as f:
+def get_run_profile(inputs_path, T, f_str):
+    # Get a list of all files VNA files
+    data_files = glob.glob(inputs_path + "*.s2p")
+
+    filenames = [os.path.basename(file) for file in data_files]
+    df = pd.DataFrame(
+        [[name] + name.split("_")[0:-1] for name in filenames],
+        columns=["name", "index", "f", "syst_T", "DR_T", "B"],
+    )
+    df["index"] = df["index"].astype(int)
+    df["f"] = df["f"].astype(int)
+    df["syst_T"] = df["syst_T"].astype(np.float64)
+    df["DR_T"] = df["DR_T"].astype(np.float64)
+    df["B"] = df["B"].astype(np.float64)
+    df.sort_values(inplace=True, by="index")
+    df.set_index("index", inplace=True)
+
+    return df
+
+
+def get_VNA_scan(filename):
+    with open(filename, "r") as f:
         # Skip header lines and read the last line as column names:
         header_lines = []
         for line in f:
-            if line.startswith('#') or line.startswith('!'):
+            if line.startswith("#") or line.startswith("!"):
                 header_lines.append(line.strip())
             else:
                 break
         column_names = header_lines[-1].split()[1:]
-        
+
         # Read data skipping the header lines:
         data = np.loadtxt(filename, skiprows=5)
-        
+
         # Construct DataFrame with column names:
         df = pd.DataFrame(data, columns=column_names)
     return df
--- main.py
+++ main.py
@@ -6,35 +6,20 @@ import os
 
 # pd.set_option('display.precision',12)
 
-inputs_root_path = 'inputs/'
-outputs_root_path = 'outputs/'
-
-# Parameters (for now single values, in the future loop over all T, and f's)
+inputs_root_path = "inputs/"
+outputs_root_path = "outputs/"
 sample_name = "FF DR001"
+
+# Run parameters (for now single values, in the future loop over all T, and f's)
 T = 50
-f_str = '1'
-
-inputs_path = inputs_root_path + str(T) + 'K/freq '+ f_str + '/'
-# Get a list of all files matching the pattern
-data_files = glob.glob(inputs_path + '*.s2p')
-
-# Building parameters df
-filenames = [os.path.basename(file) for file in data_files]
-df = pd.DataFrame([[name] + name.split('_')[0:-1] for name in filenames], columns=['name', 'index', 'f', 'syst_T', 'DR_T', 'B'])
-df['index'] = df['index'].astype(int)
-df['f'] = df['f'].astype(int)
-df['syst_T'] = df['syst_T'].astype(np.float64)
-df['DR_T'] = df['DR_T'].astype(np.float64)
-df['B'] = df['B'].astype(np.float64)
-df.sort_values(inplace=True, by='index')
-df.set_index('index', inplace=True)
-del filenames # to make sure I only get it from ordered dataframe
+f_str = "1"
+inputs_path = inputs_root_path + str(T) + "K/freq " + f_str + "/"
+df = get_run_profile(inputs_path, T, f_str)
+# print(df)
 
 # Calculate quality factor
-for file in df['name']:
-    data_df = read_touchstone(inputs_path + file)
-    [Qloaded, Qunloaded, resonance, beta1, beta2] = DR_calculation(data_df)
-    # break
-    
-
-
+for file in df["name"]:
+    data_df = get_VNA_scan(inputs_path + file)
+    [df_extended, Qloaded, Qunloaded, resonance, beta1, beta2] = DR_calculation(data_df)
+    lorentzian_fitting(df_extended, Qloaded, resonance)
+    break
