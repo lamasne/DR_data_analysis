b0f6706f32b6acbed039893f8a7c6c8faf8edc1c
Neil Lamas <nlamas@icmab-csic.icmab.es>
2024-04-11 17:37:24 +0100

    improved the linear correction. It now finds fits almost all the time + correction of X into X_cor_framed

--- old/ARPE/algorithm/Phase_Unwrap_Correction.py
+++ old/ARPE/algorithm/Phase_Unwrap_Correction.py
@@ -70,36 +70,137 @@ def fano_fitting(X, Y):
 #         print('it worked!')
 #     return popt
 
-def correct_lin_trend_phase(Y_framed):
-    '''Requires framed Y, works because plot must be symmetrical'''
-    X = np.arange(len(Y_framed))
-    Y_s = savgol_filter(Y_framed, 11, 2)
+# def correct_lin_trend_phase(Y_framed):
+#     '''Requires framed Y, works because plot must be symmetrical'''
+#     X = np.arange(len(Y_framed))
+#     Y_s = savgol_filter(Y_framed, 11, 2)
+#     dy_dx = np.gradient(Y_s, X)
+#     lin_trend = np.mean(dy_dx)
+#     Y_cor = Y_framed.copy() - lin_trend*(X - X[len(Y_framed)//2])                    
+#     plt.figure()  
+#     plt.title('Correction lin trend')
+#     plt.plot(X, Y_framed, 'b-')
+#     plt.plot(X, Y_cor, 'r-')
+#     plt.show()
+#     return Y_cor
+
+def new_correct_lin_trend_phase(Y, tol=1.5):
+    # softener = 0.5
+    softener = 1
+    X = np.arange(len(Y))
+    Y_s = savgol_filter(Y, 25, 2)
     dy_dx = np.gradient(Y_s, X)
-    lin_trend = np.mean(dy_dx)
-    Y_cor = Y_framed.copy() - lin_trend*(X - X[len(Y_framed)//2])                    
+    dy_dx = savgol_filter(dy_dx, 31, 2)
+    idx_l_end = 6
+    while abs(dy_dx[idx_l_end]/np.mean(dy_dx[:5])) < tol:
+        idx_l_end += 1
+    x_l = np.arange(idx_l_end)
+    coeff_l = np.polyfit(
+        x_l,
+        Y_s[:idx_l_end],
+        1
+    )
+    idx_r_end = len(Y)-6
+    while abs(dy_dx[idx_r_end]/np.mean(dy_dx[-5:])) < tol:
+        idx_r_end -= 1
+    x_r = np.arange(idx_r_end, len(Y_s))
+    coeff_r = np.polyfit(
+        x_r,
+        Y_s[idx_r_end:],
+        1
+    )
     plt.figure()  
-    plt.plot(X, Y_framed, 'b-')
+    plt.title('Finding the edges')
+    plt.plot(X, Y, 'b-')
+    plt.plot(x_l, Y_s[:idx_l_end], 'r-')
+    plt.plot(x_r, Y_s[idx_r_end:], 'r-')
+    lin_trend = softener*(coeff_l[0] + coeff_r[0])/2.0
+    Y_cor = Y.copy() - lin_trend*(X - X[len(Y)//2])                    
+    plt.figure()  
+    plt.title('Correction linear trend')
+    plt.plot(X, Y, 'b-')
     plt.plot(X, Y_cor, 'r-')
     plt.show()
     return Y_cor
 
-def OLD_correct_lin_trend_phase(Y, resol):
-    x_cor = np.arange(len(Y))
-    size = len(Y)//resol 
-    coef_lin_left_by_chunk = [np.polyfit(np.linspace(j*size,(j+1)*size,size), Y[j*size:(j+1)*size], 1)[0] for j in range(len(Y)//3//size)]
-    coef_lin_right_by_chunk = [np.polyfit(np.linspace(len(Y)-(j+1)*size, len(Y)-j*size,size), Y[len(Y)-(j+1)*size:len(Y)-j*size], 1)[0] for j in range(len(Y)//3//size)]
-    a = coef_lin_left_by_chunk.copy() + coef_lin_right_by_chunk.copy() 
-    lin_trend = sum(a) / len(a)
-    Y_cor = Y.copy() - lin_trend*(x_cor - x_cor[len(Y)//2])                    
-    plt.figure()  
-    plt.plot(x_cor, Y_cor, 'b-')
-    plt.show()
-    return Y_cor
+# def new_correct_lin_trend_phase(Y, tol=2e-3):
+#     X = np.arange(len(Y))
+#     # Find ends size
+#     peaks_idxs = obtain_peaks_idxs(Y)
+#     Y_s = savgol_filter(Y, 25, 2)
+#     dy_dx = np.gradient(np.gradient(Y_s, X), X)
+#     dy_dx = savgol_filter(dy_dx, 31, 2)
+#     norm_tol = abs(tol*max(dy_dx))
+#     idx_l_end = min(peaks_idxs)
+#     while abs(dy_dx[0] - dy_dx[idx_l_end]) > norm_tol:
+#         idx_l_end -= 1
+#     x_l = np.arange(idx_l_end)
+#     coeff_l = np.polyfit(
+#         x_l,
+#         Y_s[:idx_l_end],
+#         1
+#     )
+#     idx_r_end = max(peaks_idxs)
+#     while abs(dy_dx[-1] - dy_dx[idx_r_end]) > norm_tol:
+#         idx_r_end += 1
+#     x_r = np.arange(idx_r_end, len(Y_s))
+#     coeff_r = np.polyfit(
+#         x_r,
+#         Y_s[idx_r_end:],
+#         1
+#     )
+#     plt.figure()  
+#     plt.title('Finding the edges')
+#     plt.plot(X, Y, 'b-')
+#     plt.plot(x_l, Y_s[:idx_l_end], 'r-')
+#     plt.plot(x_r, Y_s[idx_r_end:], 'r-')
+#     lin_trend = (coeff_l[0] + coeff_r[0])/2.0
+#     Y_cor = Y.copy() - lin_trend*(X - X[len(Y)//2])                    
+#     plt.figure()  
+#     plt.title('Correction linear trend')
+#     plt.plot(X, Y, 'b-')
+#     plt.plot(X, Y_cor, 'r-')
+#     plt.show()
+#     return Y_cor
+
+# def OLD_correct_lin_trend_phase(Y, resol):
+#     '''Requires framed Y, works because plot must be symmetrical'''
+#     X = np.arange(len(Y))
+#     chunk_size = len(Y)//resol 
+#     ends_size = len(Y)//3//chunk_size
+#     coef_lin_left_by_chunk = [np.polyfit(np.linspace(j*chunk_size,(j+1)*chunk_size,chunk_size), Y[j*chunk_size:(j+1)*chunk_size], 1)[0] for j in range(ends_size)]
+#     coef_lin_right_by_chunk = [np.polyfit(np.linspace(len(Y)-(j+1)*chunk_size, len(Y)-j*chunk_size,chunk_size), Y[len(Y)-(j+1)*chunk_size:len(Y)-j*chunk_size], 1)[0] for j in range(ends_size)]
+#     a = coef_lin_left_by_chunk.copy() + coef_lin_right_by_chunk.copy() 
+#     lin_trend = np.mean(a)
+#     Y_cor = Y.copy() - lin_trend*(X - X[len(Y)//2])                    
+#     plt.figure()  
+#     plt.title('Correction linear trend')
+#     plt.plot(X, Y, 'b-')
+#     plt.plot(X, Y_cor, 'r-')
+#     plt.show()
+#     return Y_cor
 
 def frame_S_param_phase(Y, tol = 0.1):
+    # Get the center x - works for curve with one and 2 symetrical peaks
     X = np.arange(len(Y))
-    Y_s = savgol_filter(Y, 11, 2)
-    d2y_dx2 = np.gradient(np.gradient(Y_s, X), X)
+    peaks_idxs = obtain_peaks_idxs(Y, tol)
+    idx_Y_mid = int(np.mean(peaks_idxs))
+    nb_pts_end_curve = min([idx_Y_mid, len(Y)-idx_Y_mid])
+    Y_framed = Y[idx_Y_mid-nb_pts_end_curve:idx_Y_mid+nb_pts_end_curve].copy()
+    # plt.figure()
+    # plt.title('Framing of phase')
+    # plt.plot(Y)
+    # plt.plot(np.linspace(idx_Y_mid-nb_pts_end_curve, idx_Y_mid+nb_pts_end_curve, len(Y_framed)), Y_framed)
+    # plt.show()
+    return Y_framed
+
+def obtain_peaks_idxs(Y, tol=0.1):
+    X = np.arange(len(Y))
+    Y_s = savgol_filter(Y, 25, 2)
+    dy_dx = np.gradient(Y_s, X)
+    dy_dx = savgol_filter(dy_dx, 31, 2)
+    d2y_dx2 = np.gradient(dy_dx, X)
+    d2y_dx2 = savgol_filter(d2y_dx2, 31, 2)
     peaks_idxs = [np.argmax(abs(d2y_dx2))]
     peak_2_candidates = np.array([[i, elem] for i, elem in enumerate (d2y_dx2) if abs(d2y_dx2[peaks_idxs[0]]/abs(elem) < tol)])
     if len(peak_2_candidates) > 0:
@@ -108,79 +209,75 @@ def frame_S_param_phase(Y, tol = 0.1):
             if cand[1] > best_can[1]:
                 best_can = cand.copy()
         peaks_idxs.append(int(best_can[0]))
-    # plt.figure()
-    # plt.subplot(2, 1, 1)  
-    # plt.plot(Y, 'b*')
-    # plt.plot(Y_s, 'r-')
-    # plt.subplot(2, 1, 2) 
-    # plt.plot(d2y_dx2, 'g--')
-    # plt.plot(peaks_idxs, [d2y_dx2[i] for i in peaks_idxs], 'b*')
+    # fig, ax1 = plt.subplots()
+    # plt.title('Phase smoothing + Peak detection')
+    # ax1.plot(Y, 'b*', label='Y')
+    # ax1.plot(Y_s, 'r-', label='Y_s')
+    # ax1.set_ylabel('norm. Y and Y_s')
+    # ax2 = ax1.twinx()
+    # ax2.set_ylabel('norm. dy/dx and d2y/dx2')
+    # ax2.plot(d2y_dx2/abs(max(d2y_dx2)), 'g--', label='d2y/dx2')
+    # ax2.plot(peaks_idxs, [d2y_dx2[i]/abs(max(d2y_dx2)) for i in peaks_idxs], 'b*')
+    # ax2.plot(dy_dx/abs(max(dy_dx)), 'k--', label='dy/dx')
+    # # Combine the legends from all axes
+    # lines = ax1.get_lines() + ax2.get_lines()
+    # labels = [line.get_label() for line in lines]
+    # plt.legend(lines, labels)
     # plt.show()
-    # Get the center x - works for curve with one and 2 symetrical peaks
-    idx_Y_mid = int(np.mean(peaks_idxs))
-    nb_pts_end_curve = min([idx_Y_mid, len(Y)-idx_Y_mid])
-    Y_framed = Y[idx_Y_mid-nb_pts_end_curve:idx_Y_mid+nb_pts_end_curve].copy()
-    plt.figure('Framing of phase')
-    plt.plot(Y)
-    plt.plot(np.linspace(idx_Y_mid-nb_pts_end_curve, idx_Y_mid+nb_pts_end_curve, len(Y_framed)), Y_framed)
-    plt.show()
-    return Y_framed
+    return peaks_idxs
 
+# def OLD_frame_S_param_phase(Y, size, tol):
+#     res = None
+#     for y_zero in np.linspace(0.3, 0.7, 100): 
+#         zeros = get_zeros(Y, y_zero, size, tol)
+#         if len(zeros) == 3:
+#             if res is None:
+#                 res = [y_zero, zeros]           
+#             elif (zeros[2]-zeros[1]) * (zeros[1]-zeros[0]) < left_dist_to_mid * right_dist_to_mid:   
+#             # elif abs((zeros[2]-zeros[1]) - (zeros[1]-zeros[0])) < abs(left_dist_to_mid - right_dist_to_mid):   
+#                     res = [y_zero, zeros]             
+#             left_dist_to_mid = res[1][2]-res[1][1]
+#             right_dist_to_mid = res[1][1]-res[1][0]    
 
-def get_zeros(Y, y_zero, size, tol):
-    zeros = []
-    i = 0
-    while i < len(Y):
-        if abs(Y[i] - y_zero) < tol:
-            candidates = []
-            j = i
-            for y in Y[i:(i+size)]:
-                if abs(y - y_zero) < tol:
-                    candidates.append([y,j])
-                j+=1
-            # # transpose the matrix
-            # candidates = [list(row) for row in zip(*candidates)]
-            min_res_Y = abs(candidates[0][0] - y_zero)
-            idx_min_res_Y = candidates[0][1]
-            for candidate in candidates:
-                if abs(candidate[0] - y_zero) < min_res_Y:
-                    min_res_Y, idx_min_res_Y = candidate
-            zeros.append(idx_min_res_Y)
-            # keep looking for zeros from last candidate 
-            i = candidates[-1][1]
-        i += 1
-    return zeros    
-
-def OLD_frame_S_param_phase(Y, size, tol):
-    res = None
-    for y_zero in np.linspace(0.3, 0.7, 100): 
-        zeros = get_zeros(Y, y_zero, size, tol)
-        if len(zeros) == 3:
-            if res is None:
-                res = [y_zero, zeros]           
-            elif (zeros[2]-zeros[1]) * (zeros[1]-zeros[0]) < left_dist_to_mid * right_dist_to_mid:   
-            # elif abs((zeros[2]-zeros[1]) - (zeros[1]-zeros[0])) < abs(left_dist_to_mid - right_dist_to_mid):   
-                    res = [y_zero, zeros]             
-            left_dist_to_mid = res[1][2]-res[1][1]
-            right_dist_to_mid = res[1][1]-res[1][0]    
-
-    try:
-        idx_Y_mid = res[1][1]
-    except Exception as e:
-        plt.figure('Failed to center')
-        plt.plot(Y)
-        plt.show()
-        # raise Exception('What kind of S-param phase curve is that?')
+#     try:
+#         idx_Y_mid = res[1][1]
+#     except Exception as e:
+#         plt.figure('Failed to center')
+#         plt.plot(Y)
+#         plt.show()
+#         # raise Exception('What kind of S-param phase curve is that?')
             
-    nb_pts_end_curve = min([idx_Y_mid, len(Y)-idx_Y_mid])
-    Y_framed = Y[idx_Y_mid-nb_pts_end_curve:idx_Y_mid+nb_pts_end_curve].copy()
-    plt.figure('Framing of phase')
-    plt.plot(Y)
-    plt.plot(np.linspace(idx_Y_mid-nb_pts_end_curve, idx_Y_mid+nb_pts_end_curve, len(Y_framed)), Y_framed)
-    plt.show()
-    return Y_framed
+#     nb_pts_end_curve = min([idx_Y_mid, len(Y)-idx_Y_mid])
+#     Y_framed = Y[idx_Y_mid-nb_pts_end_curve:idx_Y_mid+nb_pts_end_curve].copy()
+#     plt.figure('Framing of phase')
+#     plt.plot(Y)
+#     plt.plot(np.linspace(idx_Y_mid-nb_pts_end_curve, idx_Y_mid+nb_pts_end_curve, len(Y_framed)), Y_framed)
+#     plt.show()
+#     return Y_framed
 
-
+# def get_zeros(Y, y_zero, size, tol):
+#     zeros = []
+#     i = 0
+#     while i < len(Y):
+#         if abs(Y[i] - y_zero) < tol:
+#             candidates = []
+#             j = i
+#             for y in Y[i:(i+size)]:
+#                 if abs(y - y_zero) < tol:
+#                     candidates.append([y,j])
+#                 j+=1
+#             # # transpose the matrix
+#             # candidates = [list(row) for row in zip(*candidates)]
+#             min_res_Y = abs(candidates[0][0] - y_zero)
+#             idx_min_res_Y = candidates[0][1]
+#             for candidate in candidates:
+#                 if abs(candidate[0] - y_zero) < min_res_Y:
+#                     min_res_Y, idx_min_res_Y = candidate
+#             zeros.append(idx_min_res_Y)
+#             # keep looking for zeros from last candidate 
+#             i = candidates[-1][1]
+#         i += 1
+#     return zeros    
 
 def PhaseUnwrappingCorrection(ring_slot,df):
 
@@ -272,26 +369,18 @@ def PhaseUnwrappingCorrection(ring_slot,df):
 
             # Normalize phase plot
             Y = (phase_unw  - min(phase_unw))/(max(phase_unw)-min(phase_unw))
+
+            Y_framed = frame_S_param_phase(Y)                   
+            Y_cor_framed = new_correct_lin_trend_phase(Y_framed)
+            X_cor_framed = np.linspace(0,1,len(Y_cor_framed))
+            popt = fano_fitting(X_cor_framed,Y_cor_framed)
+
             X = np.linspace(0,1,len(f))
-
-            try:    
-                Y_framed = frame_S_param_phase(Y)                   
-                # Y_framed = OLD_frame_S_param_phase(Y, size = 10, tol = 0.01)                   
-            except Exception as e:
-                print(e)
-                Y_cor = correct_lin_trend_phase(Y)
-                # Y_cor = OLD_correct_lin_trend_phase(Y, resol = 50)
-                Y_framed = frame_S_param_phase(Y_cor)                   
-
-            # Y_cor_framed = correct_lin_trend_phase(Y_framed)
-            Y_cor_framed = OLD_correct_lin_trend_phase(Y, resol = 50)
-
-            popt = fano_fitting(X,Y_cor_framed)
-
             if popt is None:
                 plt.figure()
                 plt.title('Could not fit the S-parameter phase with Fano curve')
                 plt.plot(X, Y, 'b-')
+                plt.plot(X_cor_framed, Y_cor_framed, 'g--')
                 plt.show()
                 raise Exception('Could not fit the S-parameter phase with Fano curve')
             
@@ -299,6 +388,7 @@ def PhaseUnwrappingCorrection(ring_slot,df):
                 [A, q, f0, g, H, y0] = popt
                 plt.figure()
                 plt.plot(X, Y, 'b.', label='Data')
+                plt.plot(X_cor_framed, Y_cor_framed, 'g--')
                 plt.plot(X, fano(X, *popt), 'r-', label=f'Fitted curve: q={q:.2f}, f0={f0:.2f}, g={g:.2f}, H={H:.2f}, y0={y0:.2f}')
                 plt.legend()    
                 plt.show()
